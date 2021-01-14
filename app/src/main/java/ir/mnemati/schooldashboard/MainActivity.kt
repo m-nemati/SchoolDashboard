9bd44e36de212b90d05030d487bc2c37cbe336a3
m-nemati <m.nemati2002@gmail.com>
2021-01-14 12:45:12 +0330

    version v1.5

--- app/src/main/java/ir/mnemati/schooldashboard/MainActivity.kt
+++ app/src/main/java/ir/mnemati/schooldashboard/MainActivity.kt
@@ -2,9 +2,13 @@ package ir.mnemati.schooldashboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,24 +20,35 @@ class MainActivity : AppCompatActivity() {
         adapterTest.addFragment(f2Fragment(), "اداری و مالی")
         adapterTest.addFragment(f1Fragment(), "دانش آموزی")
 
-
-
         val vp:ViewPager = findViewById(R.id.vp_main)
         vp.adapter = adapterTest
         vp.setCurrentItem(adapterTest.getCount());
 
         val tb:TabLayout = findViewById(R.id.tb_layout)
         tb.setupWithViewPager(vp)
-
-
-
     }
 
-    fun replaceFragment(fragment: Fragment){
-        /*
-        val fTransaction = supportFragmentManager.beginTransaction()
-        fTransaction.replace(R.id.fl_main, fragment)
-        fTransaction.commit()
-        */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //return super.onOptionsItemSelected(item)
+        if (item != null)
+            when(item.itemId){
+                R.id.menu_light -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                R.id.menu_dark -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+                R.id.menu_exit -> {
+                    finish();
+                    exitProcess(0);
+                }
+            }
+        return true
     }
 }
